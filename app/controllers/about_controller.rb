4124db23010431f23d2826b97d08184bdd09e7d5
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-02-13 13:11:23 -0500

    Fixing about us page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,7 +2,9 @@ class AboutController < ApplicationController
     def index
 
       @company_name = "The Jungle"
+
       @company_founding = "2023"
+      
       @company_address = "12 The Jungle Street, Toronto, Ontario"
 
     end
